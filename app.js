3445a4cf633da886ff49ec9af32969b5663ec65d
KDDeveloper <kddfreelance@example.com>
2022-03-16 16:58:21 +0530

    updates

--- app.js
+++ app.js
@@ -21,14 +21,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin:'https://kd-todo-notes.netlify.app/', credentials: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(function(req, res, next) {
   res.header('Content-Type', 'application/json;charset=UTF-8')
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true)
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
