439d417974af18dcf086e81eebc4eeb626a35fd5
KDDeveloper <kddfreelance@example.com>
2022-03-16 16:55:41 +0530

    updates

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
   res.header('Content-Type', 'application/json;charset=UTF-8')
-  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
