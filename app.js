db7c70e6ebc4efaa51d225c5b53a468a14d9e935
KDDeveloper <kddfreelance@example.com>
2022-03-16 22:46:27 +0530

    cookies trial

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({ origin:'http://localhost:3001/', credentials: true }));
+app.use(cors({ origin:'http://localhost:3001', credentials: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
